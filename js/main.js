2e7f834edb98c8f0cd1cd2f4e2052e2372003ebb
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2021-02-02 21:50:16 +0400

    feat: lesson 4

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 new Vue ({
     el: '#app',
     data: {
-        value: 1
+        show: true,
+        cars: [
+            {model: "BMW", speed: 250.8},
+            {model: "Audi", speed: 240.21},
+            {model: "Mercedes-Benz", speed: 350.4},
+            {model: "Ford", speed: 160.5}
+        ]
+
+
     },
     methods: {
-        increment (value) {
-            this.value = value
-            if ( value == 25 )
-                alert("Число 25")
-        }
-    },
-    computed: {
-        doubleValue () {
-            return this.value * 2
-        }
-    }
 
+    }
 });
